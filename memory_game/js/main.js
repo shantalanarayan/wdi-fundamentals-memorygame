820725351e4b17753c0579867d3053d99a94351a
Shantala Narayan <shantalanarayan@Shantalas-MacBook-Air.local>
2019-12-08 16:21:36 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,6 +25,16 @@ const cards = [
 
 var cardsInPlay = [];
 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
+    	cardElement.setAttribute('src', 'images/back.png');
+    	cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener('click', flipCard);
+    	document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
 function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -33,16 +43,17 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
